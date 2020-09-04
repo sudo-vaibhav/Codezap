31c6a0bbfdf4cca3b8a716f7fa7a68ee13e21c08
Yashi <59660649+yashi4001@users.noreply.github.com>
2020-09-04 21:21:11 +0530

    Firebase auth

---
+++ key.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDOYLD2f3rqjIUtVjIU7o2pjeahvMY-Flc",
+    authDomain: "codezap-84242.firebaseapp.com",
+    databaseURL: "https://codezap-84242.firebaseio.com",
+    projectId: "codezap-84242",
+    storageBucket: "codezap-84242.appspot.com",
+    messagingSenderId: "975541410224",
+    appId: "1:975541410224:web:84f559a01eb822f8395e75",
+    measurementId: "G-NBVVHYC6XY"
+  };
