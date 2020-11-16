6c2881274ab9fe6c29a12b5faae8f0e66a7f3d3c
Vaibhav <mailvaibhavchopra@gmail.com>
2020-11-17 01:46:32 +0530

    added judge0 submission trigger

--- backend/src/models/Submission/Submission.ts
+++ backend/src/models/Submission/Submission.ts
@@ -1,12 +1,11 @@
 import { Schema, model, Document } from 'mongoose';
 import { IUser } from '../User/User';
-import { IProblem } from '../Problem/Problem';
+import Problem, { IProblem } from '../Problem/Problem';
+import axios from 'axios';
 const submissionSchema = new Schema(
     {
         scoredPoints: {
             type: Number,
-            required: true,
-            immutable: false,
         },
         userId: {
             type: Schema.Types.ObjectId,
@@ -20,17 +19,71 @@ const submissionSchema = new Schema(
             ref: 'Problem',
             immutable: true,
         },
+        userInput: {
+            type: String,
+            immutable: true,
+        },
+        languageId: {
+            type: Number,
+            immutable: true,
+            required: true,
+        },
+        judge0SubmissionTokens: [{ type: [String], default: [] }],
     },
     {
         timestamps: true,
     },
 );
 
-export interface ISubmission extends Document {
+submissionSchema.methods.triggerJudge0 = async function () {
+    try {
+        console.log('trying to submit to judge0');
+        const submission: ISubmission = this;
+        const problem = await Problem.findById(submission.problemId).populate('testCases');
+        const testCases = problem.testCases || [];
+
+        const resp = await axios
+            .post(
+                'https://judge0.p.rapidapi.com/submissions/batch',
+                {
+                    submissions: testCases.map(testCase => {
+                        return {
+                            language_id: submission.languageId,
+                            source_code: submission.userInput,
+                            stdin: testCase.input,
+                            expected_output: testCase.output,
+                        };
+                    }),
+                },
+                {
+                    headers: {
+                        'content-type': 'application/json',
+                        'x-rapidapi-key': process.env['JUDGE_KEY'],
+                        'x-rapidapi-host': 'judge0.p.rapidapi.com',
+                    },
+                },
+            )
+            .then(resp => resp.data);
+        const newSubmission = await Submission.findById(submission._id);
+        newSubmission.judge0SubmissionTokens = resp.map((submission: { token: string }) => {
+            return submission.token;
+        });
+
+        await newSubmission.save();
+    } catch (e) {
+        console.log('error occured in doing judge0 submission\n', e);
+    }
+};
+
+export interface IBaseSubmission {
     scoredPoints: number;
     userId: IUser['_id'];
-    problem: IProblem['_id'];
+    problemId: IProblem['_id'];
+    userInput: string | null;
+    languageId: number;
+    judge0SubmissionTokens: [string];
 }
+export interface ISubmission extends IBaseSubmission, Document {}
 const Submission = model<ISubmission>('Submission', submissionSchema);
 
 export default Submission;
